bcf1cc6f6c4a5b59a9a0979704b825cee11a8f18
Montrell <jubileeinvestmentsdmv@gmail.com>
2022-12-23 01:20:52 -0500

    Start with blank index page

--- src/components/ui/page-header.tsx
+++ src/components/ui/page-header.tsx
@@ -3,18 +3,20 @@ import { useTranslation } from "next-i18next";
 interface HeaderProps {
 	pageSubHeader?: string;
 	pageHeader: string;
+	backgroundImage?: string;
 }
 
 const PageHeader: React.FC<HeaderProps> = ({
 	pageSubHeader = "text-page-explore",
 	pageHeader = "text-page-header",
+	backgroundImage = "url(/assets/images/page-header.jpg)"
 }) => {
 	const { t } = useTranslation("common");
 	return (
 		<div
 			className="flex justify-center p-6 md:p-10 2xl:p-8 relative bg-no-repeat bg-center bg-cover"
 			style={{
-				backgroundImage: "url(/assets/images/page-header.jpg)",
+				backgroundImage: backgroundImage,
 			}}
 		>
 			<div className="absolute top-0 start-0 bg-black w-full h-full opacity-50 transition-opacity duration-500 group-hover:opacity-80" />
